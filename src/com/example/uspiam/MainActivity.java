43f6433e5882823539765485564d99111a205cec
weslleymberg <weslleym.lisboa@gmail.com>
2012-12-04 09:16:47 -0200

    Create project source with all external dependencies

---
+++ src/com/example/uspiam/MainActivity.java
@@ -0,0 +1,17 @@
+
+package com.example.uspiam;
+
+
+import org.apache.cordova.*;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+}
